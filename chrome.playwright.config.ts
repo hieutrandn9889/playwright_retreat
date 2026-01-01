68556e8266d097a54d871fab51282c8e8756428b
hieutrandn9889 <hieutrandn123@gmail.com>
2026-01-01 16:41:07 +0700

    fix browser display

--- chrome.playwright.config.ts
+++ chrome.playwright.config.ts
@@ -10,7 +10,7 @@ const config: PlaywrightTestConfig = {
   retries: 0,
   use: {
     ...base.use,
-    headless: true,
+    headless: false,
     viewport: { width: 1280, height: 720 },
     ignoreHTTPSErrors: true,
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
@@ -20,16 +20,22 @@ const config: PlaywrightTestConfig = {
       args: [
         '--start-maximized',
         '--disable-blink-features=AutomationControlled',
-        '--disable-extensions',
         '--disable-sync',
-        '--disable-default-apps',
-        '--disable-plugins',
-        '--disable-images',
-        '--disable-component-extensions-with-background-pages',
         '--disable-default-apps',
         '--enable-automation=false',
         '--disable-dev-shm-usage',
-        '--single-process=false'
+        '--single-process=false',
+        '--disable-web-security',
+        '--disable-features=VizDisplayCompositor',
+        '--allow-running-insecure-content',
+        '--disable-webgl',
+        '--disable-features=TranslateUI',
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-accelerated-2d-canvas',
+        '--no-first-run',
+        '--no-zygote',
+        '--disable-gpu'
       ]
     }
   },
